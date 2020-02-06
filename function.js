0ddec6647318db3627927e5dc7274a84b1bd2801
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-02-06 03:25:40 +0200

    edit the js file

--- function.js
+++ function.js
@@ -2,14 +2,21 @@ alert("Hello! are you ready");
 var Name="";
 var Age="";
 var Major="";
-function information (Name,Age,Major)
+function information (Name,Age)
 {
     Name = prompt("what is your name ? ","abdallah shanaah"); 
     Age = prompt("how old are you ? ","23");
-    Major = prompt("what is your Major ? ","Ccomputer science ");
-    return "name :"+Name+"\n"+"Age :"+Age+"\n"+"Major :"+Major;
+    if(Age>6 && Age<=12)
+    {return "name :"+Name+"\n"+"Age :"+Age+"\n"+"Major : Middle School";}
+    else if
+    (Age>13 && Age<19){return "name :"+Name+"\n"+"Age :"+Age+"\n"+"Major : High School";}
+    else if
+    (Age>=19&& Age<24){return "name :"+Name+"\n"+"Age :"+Age+"\n"+"Major : University";}
+    else 
+    { return "name :"+Name+"\n"+"Age :"+Age+"\n"+"Major : YOU ARE EVEN BABY OR OLD TO STUDY";}
+    
 }
 
-  alert((information(Name,Age,Major)));
+  document.write(information(Name,Age));
             
       
